79a14c4fe02748ddc5bad32ef2b214fd9466c8d8
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-01-12 19:27:44 -0600

    Install html webpack plugin and use template.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
@@ -6,8 +7,15 @@ module.exports = {
         index: './src/index.js',
         print: './src/print.js',
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Output Management',
+            template: './src/template.html'
+        }),
+    ],
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true,
     },
 };
