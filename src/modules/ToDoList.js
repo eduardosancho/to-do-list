d851140b502c08de1bb4bd3ca8cfd81949549b82
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-01-13 21:48:57 -0600

    Add ability to edit task once

--- src/modules/ToDoList.js
+++ src/modules/ToDoList.js
@@ -13,12 +13,17 @@ export class ToDoList {
 
             const taskMarkup = (task) => `
                 <input type="checkbox" id="accept-${task.index}">
-                <p class="task-description">${task.description}</p>
-                <i id="remove-task-${task.index}" class="fas fa-ellipsis-v"></i>
+                <div><p class="task-description">${task.description}</p></div>
+                <i id="options-task-${task.index}" class="fas fa-ellipsis-v options"></i>
                 `;
 
             taskRow.innerHTML = taskMarkup(task);
             taskRow.classList.add('task-row');
+            taskRow.setAttribute('id', `task-row-${task.index}`)
+
+            taskRow.querySelector('div').addEventListener('click', () => {
+                this.editTask(taskRow, task.description, task.index);
+            });
 
             this.taskList.appendChild(taskRow);
         });
@@ -38,6 +43,23 @@ export class ToDoList {
             this.displayList();
             document.getElementById('new-task-description').value = '';
         }
+    }
+
+    editTask(row, oldDescription, index) {
+        row.classList.toggle('yellow');
+        row.querySelector('div').innerHTML = `
+        <form id="update-task-form">
+        <input type="text" class="yellow" id="update-task-description" value="${oldDescription}" autofocus>
+        </form>
+        `;
+        
+        const updateTaskForm = row.querySelector('form');
+        updateTaskForm.addEventListener('submit', (e) => {
+            e.preventDefault();
+            this.tasks[index].description = document.getElementById('update-task-description').value;
+            this.storeData();
+            this.displayList();
+        });
     }
 
 }
